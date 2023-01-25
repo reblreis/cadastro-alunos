b8387beded63eabd1e13f6ec7ac66ad574faada1
Regina Reis <116919509+reblreis@users.noreply.github.com>
2023-01-25 14:28:15 -0300

    Add files via upload

--- src/controllers/AlunoController.java
+++ src/controllers/AlunoController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.List;
 import java.util.Scanner;
 
 import entities.Aluno;
@@ -36,6 +37,100 @@ public class AlunoController {
 
 		catch (Exception e) {
 			System.out.println("\nFALHA AO CADASTRAR O CLIENTE");
+			e.printStackTrace();
+		}
+	}
+
+	// método para executar o fluxo de atualização de um cliente no banco de dados
+
+	public void atualizarAluno() {
+		try {
+			System.out.println("\nATUALIZAÇÃO DE ALUNOS:\n");
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("INFORME O ID DO ALUNO....: ");
+			Integer idAluno = Integer.parseInt(scanner.nextLine());
+
+			// consultando o aluno no banco de dados através do idAluno.
+			AlunoRepository alunoRepository = new AlunoRepository();
+
+			Aluno aluno = alunoRepository.findById(idAluno);
+
+			// verificando se o aluno foi encontrado no banco de dados
+			if (aluno != null) {
+
+				System.out.print("ALTERE O NOME......: ");
+
+				aluno.setNome(scanner.nextLine());
+
+				System.out.print("ALTERE A MATRICULA.....: ");
+
+				aluno.setMatricula(scanner.nextLine());
+
+				System.out.print("ALTERE O CPF.....: ");
+
+				aluno.setCpf(scanner.nextLine());
+
+				// atualizando o aluno no banco de dados alunoRepository.update(aluno);
+
+				System.out.println("\nALUNO ATUALIZADO COM SUCESSO.");
+			} else {
+				System.out.println("\nALUNO NÃO ENCONTRADO.");
+			}
+		} catch (Exception e) {
+			System.out.println("\nFALHA AO ATUALIZAR O ALUNO.");
+			e.printStackTrace();
+		}
+	}
+
+	// método para executar o fluxo de exclusão de um ALUNO no banco de dados
+
+	public void excluirAluno() {
+		try {
+			System.out.println("\nEXCLUSÃO DE ALUNOS:\n");
+
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("INFORME O ID DO ALUNO....: ");
+			Integer idAluno = Integer.parseInt(scanner.nextLine());
+
+			// consultando o cliente no banco de dados através do id..
+
+			AlunoRepository alunoRepository = new AlunoRepository();
+			Aluno aluno = alunoRepository.findById(idAluno);
+
+			// verificando se o aluno foi encontrado
+
+			if (aluno != null) {
+				// excluindo o aluno
+
+				alunoRepository.delete(aluno);
+
+				System.out.println("\nALUNO EXCLUÍDO COM SUCESSO.");
+			} else {
+				System.out.println("\nALUNO NÃO ENCONTRADO.");
+			}
+
+		} catch (Exception e) {
+			System.out.println("\nFALHA AO EXCLUIR O ALUNO.");
+			e.printStackTrace();
+		}
+	}
+
+	// método para executar o fluxo de consulta de alunos no banco de dados
+	public void consultarAlunos() {
+		try {
+			System.out.println("\nCONSULTA DE ALUNOS:\n");
+			AlunoRepository alunoRepository = new AlunoRepository();
+			List<Aluno> lista = alunoRepository.findAll();
+
+			for (Aluno aluno : lista) {
+				System.out.println("ID DO ALUNO...: " + aluno.getIdAluno());
+				System.out.println("NOME............: " + aluno.getNome());
+				System.out.println("MATRICULA...........: " + aluno.getMatricula());
+				System.out.println("CPF...........: " + aluno.getCpf());
+				System.out.println("...");
+			}
+		} catch (Exception e) {
+			System.out.println("\nFALHA AO CONSULTAR ALUNOS.");
 			e.printStackTrace();
 		}
 	}
--- src/repositories/AlunoRepository.java
+++ src/repositories/AlunoRepository.java
@@ -2,6 +2,8 @@ package repositories;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import entities.Aluno;
@@ -11,48 +13,144 @@ public class AlunoRepository {
 
 	private Connection connection;
 
-	// método para gravar um aluno no banco de dados. VOID NÃO RETORNA NADA! CREATE NÃO VAI LER NADA!
+	// método para gravar um aluno no banco de dados. VOID NÃO RETORNA NADA! CREATE
+	// NÃO VAI LER NADA!
 
 	public void create(Aluno aluno) throws Exception {
 
 		// abrindo conexão com o banco de dados
 
 		Connection connection = ConnectionFactory.createConnection();
-	
-	// executando um comando SQL no banco de dados para cadastrar o cliente
 
-	PreparedStatement preparedStatement = connection
-			.prepareStatement("insert into aluno(nome, matricula, cpf) values (?, ?, ?)");
+		// executando um comando SQL no banco de dados para cadastrar o cliente
 
-	// passando os parametros do comando SQL
+		PreparedStatement preparedStatement = connection
+				.prepareStatement("insert into aluno(nome, matricula, cpf) values (?, ?, ?)");
+
+		// passando os parametros do comando SQL
 		preparedStatement.setString(1, aluno.getNome());
 		preparedStatement.setString(2, aluno.getMatricula());
 		preparedStatement.setString(3, aluno.getCpf());
 
-	// executar o comando SQL e fechar a conexão com o banco de dados
+		// executar o comando SQL e fechar a conexão com o banco de dados
 		preparedStatement.execute();
 		connection.close();
 	}
-	
-	// método para atualizar os dados de um cliente no banco de dados. VOID NÃO
+
+	// método para atualizar os dados de um aluno no banco de dados. VOID NÃO
 	// RETORNA NADA! UPDATE NÃO VAI LER NADA!
 
 	public void update(Aluno aluno) throws Exception {
-		// TODO
+		// abrindo conexão com o banco de dados
+		Connection connection = ConnectionFactory.createConnection();
+
+		// executando um comando SQL no banco de dados para atualizar o aluno
+		PreparedStatement preparedStatement = connection
+				.prepareStatement("update aluno set nome=?, matricula=?, cpf=?, where idaluno=?");
+
+		// passando os parametros do comando SQL
+		preparedStatement.setString(1, aluno.getNome());
+		preparedStatement.setString(2, aluno.getMatricula());
+		preparedStatement.setString(3, aluno.getCpf());
+		preparedStatement.setInt(4, aluno.getIdAluno());
+
+		// executar o comando SQL e fechar a conexão com o banco de dados
+		preparedStatement.execute();
+		connection.close();
+
 	}
 
-	// método para excluir um cliente no banco de dados. VOID NÃO RETORNA NADA!
-	// DELETE NÃO VAI LER NADA!
+	// método para excluir um aluno no banco de dados. VOID NÃO RETORNA NADA! DELETE
+	// NÃO VAI LER NADA!
 
 	public void delete(Aluno aluno) throws Exception {
-		// TODO
+
+		// abrindo conexão com o banco de dados
+		Connection connection = ConnectionFactory.createConnection();
+
+		// executando um comando SQL no banco de dados para excluir o cliente
+		PreparedStatement preparedStatement = connection.prepareStatement("delete from aluno where idaluno=?");
+
+		// passando os parametros do comando SQL
+		preparedStatement.setInt(1, aluno.getIdAluno());
+
+		// executar o comando SQL e fechar a conexão com o banco de dados
+		preparedStatement.execute();
+		connection.close();
+
 	}
 
-	// método para retornar todos os clientes cadastrados no banco de dados.
+	// método para retornar todos os alunos cadastrados no banco de dados.
 	// TRABALHA COM MUITOS = LISTA DE ALUNOS
 
 	public List<Aluno> findAll() throws Exception {
-		// TODO
-		return null; // NULL = PROVISÓRIO
+
+		// abrindo conexão com o banco de dados
+		Connection connection = ConnectionFactory.createConnection();
+
+		// executando um comando SQL no banco
+		// de dados para consultar os clientes
+		PreparedStatement preparedStatement = connection.prepareStatement("select * from aluno");
+
+		// ler e armazenar os registros obtidos do banco de dados
+		ResultSet resultSet = preparedStatement.executeQuery();
+
+		// declarando uma lista de clientes vazia
+		List<Aluno> lista = new ArrayList<Aluno>();
+
+		// percorrendo cada registro obtido do banco de dados
+		while (resultSet.next()) {
+
+			Aluno aluno = new Aluno();
+
+			aluno.setIdAluno(resultSet.getInt("idaluno"));
+			aluno.setNome(resultSet.getString("nome"));
+			aluno.setMatricula(resultSet.getString("matricula"));
+			aluno.setCpf(resultSet.getString("cpf"));
+
+			lista.add(aluno); // adicionar o cliente na lista
+		}
+
+		// fechando a conexão com o banco de dados
+		connection.close();
+
+		// retornando a lista
+		return lista;
+
+		// return null; NULL = PROVISÓRIO
 	}
+
+	// método para retornar 1 aluno cadastrados no banco de dados através do ID
+	public Aluno findById(Integer idAluno) throws Exception {
+
+		// abrindo conexão com o banco de dados
+		Connection connection = ConnectionFactory.createConnection();
+
+		// de dados para consultar 1 aluno através do ID
+		PreparedStatement preparedStatement = connection.prepareStatement("select * from aluno where idaluno=?");
+		preparedStatement.setInt(1, idAluno);
+
+		// ler e armazenar os registros obtidos do banco de dados
+		ResultSet resultSet = preparedStatement.executeQuery();
+
+		// criando um objeto Aluno vazio
+		Aluno aluno = null;
+
+		// verificando se algum aluno foi encontrado no banco de dados
+		if (resultSet.next()) {
+			aluno = new Aluno();
+
+			aluno.setIdAluno(resultSet.getInt("idaluno"));
+			aluno.setNome(resultSet.getString("nome"));
+			aluno.setMatricula(resultSet.getString("matricula"));
+			aluno.setCpf(resultSet.getString("cpf"));
+		}
+
+		// fechando a conexão com o banco de dados
+		connection.close();
+
+		// retornando o aluno
+		return aluno;
+	}
+
 }
